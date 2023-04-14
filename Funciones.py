28e67be3fb4d1501143c746f6ca363ba8cd5da93
12Rocha <123138385+12Rocha@users.noreply.github.com>
2023-04-14 13:41:38 -0500

    Add files via upload

---
+++ Funciones.py
@@ -0,0 +1,15 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero")
+    return a / b
+
+print(return a+b)
